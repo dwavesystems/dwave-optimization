a1e92f113330159f2fa934883857aef300eeaad2
Jeremy Woo <166665702+jwoo098@users.noreply.github.com>
2024-08-16 11:34:28 +0900

    fix test case for exponent of 1

--- tests/test_model.py
+++ tests/test_model.py
@@ -72,7 +72,7 @@ class TestArraySymbol(unittest.TestCase):
         # The operators that don't fit as neatly into the above
 
         with self.subTest("__pow__"):
-            self.assertEqual(x ** 1, x)
+            self.assertIsInstance(x ** 1, type(i))
             self.assertIsInstance(x ** 2, dwave.optimization.symbols.Square)
             self.assertIsInstance(x ** 3, dwave.optimization.model.ArraySymbol)
             self.assertIsInstance(x ** 4, dwave.optimization.model.ArraySymbol)
