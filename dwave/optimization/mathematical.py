568415e65ddd37c6026bdf3f045ab0632af27efb
Samuel Bodin <samuel.bodin@live.se>
2025-01-10 15:57:41 +0100

    Add stack function with tests

--- dwave/optimization/mathematical.py
+++ dwave/optimization/mathematical.py
@@ -53,6 +53,7 @@ __all__ = [
     "mod",
     "multiply",
     "sqrt",
+    "stack",
     "where",
 ]
 
@@ -539,6 +540,60 @@ def sqrt(x: ArraySymbol) -> SquareRoot:
         :class:`~dwave.optimization.symbols.SquareRoot`: equivalent symbol.
     """
     return SquareRoot(x)
+
+
+def stack(xi: collections.abc.Iterable, axis : int = 0) -> ArraySymbol:
+    r"""Joins a sequence of ArraySymbols along a new axis.
+
+        Args:
+            xi: sequence of ArraySymbol
+        Returns:
+            The joined ArraySymbols on a new axis
+        Examples:
+            This example stacks three scalars on the first axis.
+            >>> from dwave.optimization import Model, stack
+            ...
+            >>> model = Model()
+            >>> x = [model.constant(1), model.constant(2), model.constant(3)]
+            >>> s = stack(x)
+            >>> s.shape()
+            (3,)
+            >>> with model.lock():
+            ...     model.states.resize(1)
+            ...     print(s.state(0))
+            [1. 2. 3.]
+
+            This example stacks three 1d arrays on axis 0 and 1
+            >>> from dwave.optimization import Model, stack
+            ...
+            >>> model = Model()
+            >>> a = model.constant([1,2])
+            >>> b = model.constant([3,4])
+            >>> c = model.constant([5,6])
+            >>> s0 = stack((a,b,c), axis=0)
+            >>> s0.shape()
+            (3,2)
+            >>> s1 = stack((a,b,c), axis=1)
+            >>> s1.shape()
+            (2,3)
+            >>> with model.lock():
+            ...     model.states.resize(1)
+            ...     print(s0.state(0))
+            ...     print(s1.state(0))
+            [[1. 2.]
+             [3. 4.]
+             [5. 6.]]
+            [[1. 3. 5.]
+             [2. 4. 6.]]
+    """
+    if all([ isinstance(x, ArraySymbol) for x in xi]):
+        if not (0 <= axis and axis <= xi[0].ndim()):
+            raise ValueError(f"axis {axis} is out of bounds for array of dimension {xi[0].ndim()+1}")
+
+        make_shape = lambda x: (x.shape()[:axis]) + (1,) + (x.shape()[axis:])
+        return concatenate([ x.reshape(make_shape(x)) for x in xi ], axis)
+
+    raise ValueError("stack takes an Iterable of ArraySymbols of same shape")
 
 
 def where(condition: ArraySymbol, x: ArraySymbol, y: ArraySymbol) -> Where:
