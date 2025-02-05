9df7780434af4ee5410b776da27b502a1776c4de
William Bernoudy <wbernoudy@dwavesys.com>
2025-02-05 14:58:26 -0800

    Simplex: Fix bug in handling lower bounded constraints

--- tests/cpp/test_simplex.cpp
+++ tests/cpp/test_simplex.cpp
@@ -13,26 +13,22 @@
 //    limitations under the License.
 
 #include <algorithm>
-
-#include "catch2/catch_test_macros.hpp"
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
 
+#include "catch2/catch_test_macros.hpp"
 #include "simplex.hpp"
 
 namespace dwave::optimization {
 
 using Catch::Matchers::WithinRel;
 
-TEST_CASE("LP solver (simplex)") {
+TEST_CASE("LP solver (simplex)", "[simplex]") {
     static const double inf = std::numeric_limits<double>::infinity();
 
     GIVEN("A simple LP with only upper bounds on A @ x") {
         std::vector<double> c{-2, -3, -4};
 
-        std::vector<double> A{
-            3, 2, 1,
-            2, 5, 3
-        };
+        std::vector<double> A{3, 2, 1, 2, 5, 3};
 
         std::vector<double> b_lb{-inf, -inf};
         std::vector<double> b_ub{10, 15};
@@ -57,15 +53,16 @@ TEST_CASE("LP solver (simplex)") {
         std::vector<double> c{-5, -10, -3, -4};
 
         std::vector<double> A{
-            1, 1, 1, 1,
-            0, 5, -3, 0,
-            1, 2, 3, 4,
+                1, 1, 1, 1, 0, 5, -3, 0, 1, 2, 3, 4,
         };
         std::vector<double> b_lb{-inf, -inf, -inf};
         std::vector<double> b_ub{100, 50, 20};
 
         std::vector<double> A_eq{
-            3, 0, 0, -1,
+                3,
+                0,
+                0,
+                -1,
         };
         std::vector<double> b_eq{7};
 
@@ -90,13 +87,17 @@ TEST_CASE("LP solver (simplex)") {
         std::vector<double> c{-5, -10, -3};
 
         std::vector<double> A{
-            1, 1, 0,
+                1,
+                1,
+                0,
         };
         std::vector<double> b_lb{-inf};
         std::vector<double> b_ub{10};
 
         std::vector<double> A_eq{
-            1, 0, 1,
+                1,
+                0,
+                1,
         };
         std::vector<double> b_eq{7};
 
@@ -111,6 +112,62 @@ TEST_CASE("LP solver (simplex)") {
             CHECK(result.objective() == -465);
 
             CHECK(std::ranges::equal(std::vector{-43, 53, 50}, result.solution()));
+        }
+    }
+
+    GIVEN("Simple LP with one lower bounded constraint") {
+        std::vector<double> c{1, 2, 3};
+
+        std::vector<double> A{
+                1,
+                1,
+                1,
+        };
+        std::vector<double> b_lb{-2};
+        std::vector<double> b_ub{inf};
+
+        std::vector<double> A_eq{};
+        std::vector<double> b_eq{};
+
+        std::vector<double> lb{-10, -10, -10};
+        std::vector<double> ub{1, 1, 1};
+
+        THEN("We find the correct solution") {
+            SolveResult result = linprog(c, b_lb, A, b_ub, A_eq, b_eq, lb, ub);
+            CHECK(result.solve_status == SolveResult::SolveStatus::SUCCESS);
+            CHECK(result.solution_status() == SolveResult::SolutionStatus::OPTIMAL);
+            CHECK(result.feasible());
+            CHECK(result.objective() == -9);
+
+            CHECK(std::ranges::equal(std::vector{1, 1, -4}, result.solution()));
+        }
+    }
+
+    GIVEN("Simple LP with one double bounded constraint") {
+        std::vector<double> c{1, 2, 3};
+
+        std::vector<double> A{
+                1,
+                1,
+                1,
+        };
+        std::vector<double> b_lb{-2};
+        std::vector<double> b_ub{2};
+
+        std::vector<double> A_eq{};
+        std::vector<double> b_eq{};
+
+        std::vector<double> lb{-10, -10, -10};
+        std::vector<double> ub{1, 1, 1};
+
+        THEN("We find the correct solution") {
+            SolveResult result = linprog(c, b_lb, A, b_ub, A_eq, b_eq, lb, ub);
+            CHECK(result.solve_status == SolveResult::SolveStatus::SUCCESS);
+            CHECK(result.solution_status() == SolveResult::SolutionStatus::OPTIMAL);
+            CHECK(result.feasible());
+            CHECK(result.objective() == -9);
+
+            CHECK(std::ranges::equal(std::vector{1, 1, -4}, result.solution()));
         }
     }
 }
