c765681b8b3ccc39441934ae6eac25342bc6ade5
Elham Azadbakht <elham.azdbkht@gmail.com>
2025-03-07 10:35:15 -0800

    Fix the date

--- tests/cpp/nodes/test_interpolation.cpp
+++ tests/cpp/nodes/test_interpolation.cpp
@@ -1,4 +1,4 @@
-// Copyright 2023 D-Wave Inc.
+// Copyright 2025 D-Wave Inc.
 //
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
