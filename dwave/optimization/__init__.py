0079746ddc1e81f27daed898f42da00cb7fff1ea
Alexander Condello <arcondello@gmail.com>
2025-06-03 09:08:45 -0700

    Update version 0.6.2 -> 0.6.3.dev

--- dwave/optimization/__init__.py
+++ dwave/optimization/__init__.py
@@ -17,7 +17,7 @@ import dwave.optimization.generators
 from dwave.optimization.model import Model
 from dwave.optimization.mathematical import *
 
-__version__ = "0.6.2"
+__version__ = "0.6.3.dev"
 
 
 def get_include() -> str:
