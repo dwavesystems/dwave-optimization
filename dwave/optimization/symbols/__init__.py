1f0f55134d1dfffe5ae1e2965f3eed7eba462116
Alexander Condello <arcondello@gmail.com>
2025-09-15 09:47:37 -0700

    Make symbols submodule into a subpackage

---
+++ dwave/optimization/symbols/__init__.py
@@ -0,0 +1,90 @@
+# Copyright 2025 D-Wave
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+from dwave.optimization.symbols._deprecated import *
+from dwave.optimization.symbols._deprecated import _ArrayValidation
+
+
+__all__ = [
+    "Absolute",
+    "AccumulateZip",
+    "Add",
+    "All",
+    "And",
+    "Any",
+    "AdvancedIndexing",
+    "ARange",
+    "ArgSort",
+    "BasicIndexing",
+    "BinaryVariable",
+    "BroadcastTo",
+    "BSpline",
+    "Concatenate",
+    "Constant",
+    "Copy",
+    "Cos",
+    "DisjointBitSets",
+    "DisjointBitSet",
+    "DisjointLists",
+    "DisjointList",
+    "Divide",
+    "Equal",
+    "Exp",
+    "Expit",
+    "Extract",
+    "Input",
+    "IntegerVariable",
+    "LessEqual",
+    "LinearProgram",
+    "LinearProgramFeasible",
+    "LinearProgramObjectiveValue",
+    "LinearProgramSolution",
+    "ListVariable",
+    "Log",
+    "Logical",
+    "Max",
+    "Maximum",
+    "Mean",
+    "Min",
+    "Minimum",
+    "Modulus",
+    "Multiply",
+    "NaryAdd",
+    "NaryMaximum",
+    "NaryMinimum",
+    "NaryMultiply",
+    "Negative",
+    "Not",
+    "Or",
+    "PartialProd",
+    "PartialSum",
+    "Permutation",
+    "Prod",
+    "Put",
+    "QuadraticModel",
+    "Reshape",
+    "Resize",
+    "Sin",
+    "Subtract",
+    "SetVariable",
+    "Size",
+    "Rint",
+    "SafeDivide",
+    "SoftMax",
+    "Square",
+    "SquareRoot",
+    "Sum",
+    "Where",
+    "Xor",
+]
