17b37dc6d81791811f69b54942aa2cfad3603265
Alexander Condello <arcondello@gmail.com>
2025-09-16 14:45:26 -0700

    Make dwave.optimization.symbols.quadratic_model sub-module

--- dwave/optimization/symbols/__init__.py
+++ dwave/optimization/symbols/__init__.py
@@ -25,6 +25,7 @@ from dwave.optimization.symbols.lp import *
 from dwave.optimization.symbols.manipulation import *
 from dwave.optimization.symbols.naryop import *
 from dwave.optimization.symbols.numbers import *
+from dwave.optimization.symbols.quadratic_model import *
 from dwave.optimization.symbols.reduce import *
 from dwave.optimization.symbols.softmax import *
 from dwave.optimization.symbols.sorting import *
