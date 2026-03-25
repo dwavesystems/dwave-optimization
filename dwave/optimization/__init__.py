72faa63c547cb384abcdd73880b0a08a03870061
William Bernoudy <wbernoudy@dwavesys.com>
2026-03-25 12:35:44 -0700

    Update version 0.6.12.dev -> 0.6.12

--- dwave/optimization/__init__.py
+++ dwave/optimization/__init__.py
@@ -18,7 +18,7 @@ from dwave.optimization.expression import *
 from dwave.optimization.model import Model
 from dwave.optimization.mathematical import *
 
-__version__ = "0.6.12.dev"
+__version__ = "0.6.12"
 
 
 def get_include() -> str:
