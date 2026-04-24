c9999220ef55a31de4004e8ee38fd5819fecda4a
fastbodin <aslobodin@dwavesys.com>
2026-04-24 09:08:41 -0700

    Update `symbols/numbers.pyi`
    To include all public methods of `BinaryVariable` and `IntegerVariable`.

--- dwave/optimization/symbols/numbers.pyi
+++ dwave/optimization/symbols/numbers.pyi
@@ -12,12 +12,40 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-import numpy as _np
-
+import collections.abc
+import numpy as np
+import typing
 from dwave.optimization.model import ArraySymbol as _ArraySymbol
+from dwave.optimization._model import _Graph
+
+_ShapeLike: typing.TypeAlias = typing.Union[int, collections.abc.Sequence[int]]
+_SumConstraint: typing.TypeAlias = list[typing.Union[tuple[list[str], list[float]], 
+                                        tuple[int, list[str], list[float]]]]
+_AxesSubjetTo: typing.TypeAlias = list[tuple[int, str | list[str], float | list[float]]]
 
 class BinaryVariable(_ArraySymbol):
-    def set_state(self, index: int, state: _np.typing.ArrayLike): ...
+    def __init__(self, 
+                 model: _Graph,
+                 shape: None | _ShapeLike = None, 
+                 lower_bound: None | np.typing.ArrayLike = None,
+                 upper_bound: None | np.typing.ArrayLike = None,
+                 subject_to: None | list[tuple[str, float]] = None,
+                 axes_subject_to: None | _AxesSubjetTo = None): ...
+    def lower_bound(self) -> np.typing.NDArray[np.double]: ...
+    def set_state(self, index: int, state: np.typing.ArrayLike): ...
+    def sum_constraints(self) -> _SumConstraint: ...
+    def upper_bound(self) -> np.typing.NDArray[np.double]: ...
 
 class IntegerVariable(_ArraySymbol):
-    def set_state(self, index: int, state: _np.typing.ArrayLike): ...
+    def __init__(self, 
+                 model: _Graph,
+                 shape: None | _ShapeLike = None, 
+                 lower_bound: None | np.typing.ArrayLike = None,
+                 upper_bound: None | np.typing.ArrayLike = None,
+                 subject_to: None | list[tuple[str, float]] = None,
+                 axes_subject_to: None | _AxesSubjetTo = None): ...
+    def lower_bound(self) -> np.typing.NDArray[np.double]: ...
+    def set_state(self, index: int, state: np.typing.ArrayLike): ...
+    def sum_constraints(self) -> _SumConstraint: ...
+    def upper_bound(self) -> np.typing.NDArray[np.double]: ...
+
