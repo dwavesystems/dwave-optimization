404ee867cc87d876d253b73d6874fe5869a8c7ac
azucca <azucca@dwavesys.com>
2026-04-29 12:59:21 -0700

    Add test for int intervals

--- tests/cpp/cp/test_interval.cpp
+++ tests/cpp/cp/test_interval.cpp
@@ -250,4 +250,226 @@ TEST_CASE("Interval double (dynamic size)") {
     }
 }
 
+TEST_CASE("Interval Integer (static size)") {
+    GIVEN("A state manager and 10 intervals with default boundary") {
+        std::unique_ptr<Copier> sm = std::make_unique<Copier>();
+        IntIntervalArray interval = IntIntervalArray(sm.get(), 10);
+        REQUIRE(interval.num_domains() == 10);
+        REQUIRE(interval.max_size() == 10);
+        REQUIRE(interval.min_size() == 10);
+
+        // store size should be 2 x 10 + 2 for min and max size
+        REQUIRE(sm->store.size() == 22);
+        for (size_t i = 0; i < interval.num_domains(); ++i) {
+            REQUIRE(interval.min(i) == 0);
+            REQUIRE(interval.max(i) == (ssize_t)1 << 51);
+            REQUIRE(interval.size(i) == ((ssize_t)1 << 51) + 1);
+        }
+
+        AND_GIVEN("A test domain listener") {
+            std::unique_ptr<TestListener> tl = std::make_unique<TestListener>();
+            TestListener* tl_ptr = tl.get();
+
+            WHEN("We remove below 10.5 from all indices") {
+                for (int i = 0; i < 10; ++i) {
+                    auto status = interval.remove_below(10.5, i, tl_ptr);
+                    REQUIRE(status == CPStatus::OK);
+                }
+
+                THEN("The minimum is correctly changed and the maximum is left unchanged") {
+                    for (int i = 0; i < 10; ++i) {
+                        REQUIRE(interval.min(i) == 11);
+                        REQUIRE(interval.max(i) == (ssize_t)1 << 51);
+                    }
+                }
+
+                AND_WHEN("We remove above 30.5 from the fourth index") {
+                    auto status = interval.remove_above(30.3, 4, tl_ptr);
+                    REQUIRE(status == CPStatus::OK);
+                    REQUIRE(interval.max(4) == 30);
+
+                    THEN("The other indices are unchanged") {
+                        for (int i = 0; i < 10; ++i) {
+                            if (i == 4) continue;
+                            REQUIRE(interval.min(i) == 11);
+                            REQUIRE(interval.max(i) == (ssize_t)1 << 51);
+                        }
+                    }
+
+                    AND_WHEN("We remove above -3 from the first index") {
+                        auto status = interval.remove_above(-3, 1, tl_ptr);
+                        REQUIRE(status == CPStatus::Inconsistency);
+                    }
+                }
+            }
+        }
+    }
+
+    GIVEN("An array of intervals initialized by two vectors") {
+        std::unique_ptr<Copier> sm = std::make_unique<Copier>();
+        std::vector<ssize_t> lb(10);
+        std::vector<ssize_t> ub(10);
+
+        for (int i = 0; i < 10; ++i) {
+            lb[i] = -i;
+            ub[i] = i;
+        }
+
+        IntIntervalArray interval2 = IntIntervalArray(sm.get(), lb, ub);
+        REQUIRE(sm->store.size() == 22);
+        THEN("The fomains are correctly initialized") {
+            REQUIRE(interval2.num_domains() == lb.size());
+            REQUIRE(interval2.min_size() == 10);
+            REQUIRE(interval2.max_size() == 10);
+            for (int i = 0; i < 10; ++i) {
+                REQUIRE(interval2.min(i) == -i);
+                REQUIRE(interval2.max(i) == i);
+                REQUIRE(interval2.size(i) == 2 * i + 1);
+            }
+        }
+    }
+
+    GIVEN("An array of 15 intervals between 1 and 5") {
+        std::unique_ptr<Copier> sm = std::make_unique<Copier>();
+        IntIntervalArray interval3 = IntIntervalArray(sm.get(), 15, 1, 5);
+        REQUIRE(sm->store.size() == 32);
+        THEN("The domains are correctly set") {
+            REQUIRE(interval3.num_domains() == 15);
+            REQUIRE(interval3.min_size() == 15);
+            REQUIRE(interval3.max_size() == 15);
+            for (int i = 0; i < 15; ++i) {
+                REQUIRE(interval3.min(i) == 1);
+                REQUIRE(interval3.max(i) == 5);
+                REQUIRE(interval3.size(i) == 5);
+            }
+        }
+    }
+}
+
+TEST_CASE("Interval integer (dynamic size)") {
+    GIVEN("A state manager and a dynamic array with minimum size 6 and max size 10") {
+        std::unique_ptr<Copier> sm = std::make_unique<Copier>();
+        IntIntervalArray interval = IntIntervalArray(sm.get(), 6, 10);
+        REQUIRE(interval.num_domains() == 10);
+        REQUIRE(interval.min_size() == 6);
+        REQUIRE(interval.max_size() == 10);
+        // store size should be 2 x 10 + 2 for min and max
+        REQUIRE(sm->store.size() == 22);
+        for (size_t i = 0; i < interval.num_domains(); ++i) {
+            REQUIRE(interval.min(i) == 0);
+            REQUIRE(interval.max(i) == (ssize_t)1 << 51);
+            REQUIRE(interval.size(i) == ((ssize_t)1 << 51) + 1);
+        }
+
+        // Check the active and optional entries
+        for (size_t i = 0; i < 6; ++i) {
+            REQUIRE(interval.is_active(i));
+        }
+        for (size_t i = 7; i < 10; ++i) {
+            REQUIRE_FALSE(interval.is_active(i));
+            REQUIRE(interval.maybe_active(i));
+        }
+
+        AND_GIVEN("A test domain listener") {
+            std::unique_ptr<TestListener> tl = std::make_unique<TestListener>();
+            TestListener* tl_ptr = tl.get();
+
+            WHEN("We remove below 10.5 from all indices") {
+                for (int i = 0; i < 10; ++i) {
+                    auto status = interval.remove_below(10.5, i, tl_ptr);
+                    REQUIRE(status == CPStatus::OK);
+                }
+
+                THEN("The minimum is correctly changed and the maximum is left unchanged") {
+                    for (int i = 0; i < 10; ++i) {
+                        REQUIRE(interval.min(i) == 11);
+                        REQUIRE(interval.max(i) == (ssize_t)1 << 51);
+                    }
+                }
+
+                AND_WHEN("We remove above 31.6 from the fourth index") {
+                    auto status = interval.remove_above(31.6, 4, tl_ptr);
+                    REQUIRE(status == CPStatus::OK);
+                    REQUIRE(interval.max(4) == 31);
+
+                    THEN("The other indices are unchanged") {
+                        for (int i = 0; i < 10; ++i) {
+                            if (i == 4) continue;
+                            REQUIRE(interval.min(i) == 11);
+                            REQUIRE(interval.max(i) == (ssize_t)1 << 51);
+                        }
+                    }
+
+                    AND_WHEN("We remove above -3 from the first index") {
+                        auto status = interval.remove_above(-3, 1, tl_ptr);
+                        REQUIRE(status == CPStatus::Inconsistency);
+                    }
+
+                    AND_WHEN("We remove above -3 from the 7th index") {
+                        auto status = interval.remove_above(-3, 7, tl_ptr);
+                        REQUIRE(status == CPStatus::OK);
+                        REQUIRE(interval.max_size() == 7);
+                        for (int i = 7; i < 10; ++i) {
+                            REQUIRE_FALSE(interval.is_active(i));
+                            REQUIRE_FALSE(interval.maybe_active(i));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    GIVEN("An array of intervals initialized by two vectors") {
+        std::unique_ptr<Copier> sm = std::make_unique<Copier>();
+        std::vector<ssize_t> lb(10);
+        std::vector<ssize_t> ub(10);
+
+        for (int i = 0; i < 10; ++i) {
+            lb[i] = -i;
+            ub[i] = i;
+        }
+
+        IntIntervalArray interval2 = IntIntervalArray(sm.get(), 6, lb, ub);
+        REQUIRE(sm->store.size() == 22);
+        THEN("The domains are correctly initialized") {
+            REQUIRE(interval2.num_domains() == lb.size());
+            REQUIRE(interval2.min_size() == 6);
+            REQUIRE(interval2.max_size() == static_cast<ssize_t>(lb.size()));
+            for (int i = 0; i < 10; ++i) {
+                REQUIRE(interval2.min(i) == -i);
+                REQUIRE(interval2.max(i) == i);
+                REQUIRE(interval2.size(i) == 2 * i + 1);
+                if (i < 6) {
+                    REQUIRE(interval2.is_active(i));
+                } else {
+                    REQUIRE_FALSE(interval2.is_active(i));
+                    REQUIRE(interval2.maybe_active(i));
+                }
+            }
+        }
+    }
+
+    GIVEN("An array of 15 intervals between 1 and 5") {
+        std::unique_ptr<Copier> sm = std::make_unique<Copier>();
+        IntIntervalArray interval3 = IntIntervalArray(sm.get(), 6, 15, 1, 5);
+        REQUIRE(sm->store.size() == 32);
+        THEN("The domains are correctly set") {
+            REQUIRE(interval3.num_domains() == 15);
+            REQUIRE(interval3.min_size() == 6);
+            REQUIRE(interval3.max_size() == 15);
+            for (int i = 0; i < 15; ++i) {
+                REQUIRE(interval3.min(i) == 1);
+                REQUIRE(interval3.max(i) == 5);
+                REQUIRE(interval3.size(i) == 5);
+                if (i < 6) {
+                    REQUIRE(interval3.is_active(i));
+                } else {
+                    REQUIRE_FALSE(interval3.is_active(i));
+                    REQUIRE(interval3.maybe_active(i));
+                }
+            }
+        }
+    }
+}
+
 }  // namespace dwave::optimization::cp
